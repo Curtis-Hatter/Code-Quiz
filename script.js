1bca7fad95648d6cd6ac441a6ab5358c90cf159d
Curtis H <sitrucrettah1@gmail.com>
2020-11-23 22:38:36 -0800

    Create final Form after quiz is concluded: content change after set time concludes

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
 var timer = document.querySelector("#Timer");
-var time = 60;
+var questions = document.querySelector("#Questions");
+var highScore = document.querySelector("#highScore");
+
+var userScore = 18;
+var time = 2;
 
 //Setting timer for Quiz
-function startTimer() {
-    setInterval(function () {
+var countDown = setInterval(function () {
+    time--;
+    timer.textContent = "Timer: " + time + " seconds";
+    if (time <= 0) {
+        time = 0;
         timer.textContent = "Timer: " + time + " seconds";
-        time--;
-        if (time <= 0) {
-            time = 0;
-            timer.textContent = "Timer: " + time + " seconds";
-        }
-    }, 1000)
+        formCreator()
+        clearInterval(countDown);
+        return;
+    }
+}, 1000)
+
+//create the final form after quiz is concluded
+function formCreator() {
+    questions.textContent = "All Done!";
+
+    //checking for nodes
+    console.log(highScore.childElementCount)
+    highScore.innerHTML = "<h3> Your final score is: </h3>";
+
+    var form = document.createElement("form");
+    form.textContent = "Hello World";
+    highScore.appendChild(form);
+
+    //recheck for nodes
+    console.log(highScore.childElementCount);
+
 }
 
-startTimer();
+// countDown;
