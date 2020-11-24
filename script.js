dac4a215adbb9c447943861719a325228e6c97bb
Curtis H <sitrucrettah1@gmail.com>
2020-11-23 23:09:35 -0800

    Form Create function(): Add comments, create form, divx3, input, button; append childs; SKELETON OF FUNCTION COMPLETE

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var questions = document.querySelector("#Questions");
 var highScore = document.querySelector("#highScore");
 
 var userScore = 18;
-var time = 2;
+var time = 4;
 
 //Setting timer for Quiz
 var countDown = setInterval(function () {
@@ -24,15 +24,41 @@ function formCreator() {
 
     //checking for nodes
     console.log(highScore.childElementCount)
-    highScore.innerHTML = "<h3> Your final score is: </h3>";
 
+    //changing innerHTML for Content
+    highScore.innerHTML = "<h3> Your final score is: " + userScore + "!</h3>";
+
+    //create all childs for Highscore form
     var form = document.createElement("form");
-    form.textContent = "Hello World";
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    var div3 = document.createElement("div");
+    var input1 = document.createElement("input");
+    var button = document.createElement("button");
+
+    //create attributes for div elements
+    div1.setAttribute("class", "form-row");
+    div2.setAttribute("class", "col-9");
+    div3.setAttribute("class", "col-3");
+
+    //create attributes for input elements
+    input1.setAttribute("type", "text");
+    input1.setAttribute("class", "form-control");
+    input1.setAttribute("placeholder", "Welcome to the Leaderboards");
+    button.setAttribute("type", "button");
+    button.setAttribute("class", "btn btn-primary")
+    button.textContent = "Submit";
+
+    //child appending to create form
+    div2.appendChild(input1);
+    div3.appendChild(button);
+    div1.appendChild(div2);
+    div1.appendChild(div3);
+    form.appendChild(div1);
     highScore.appendChild(form);
 
     //recheck for nodes
     console.log(highScore.childElementCount);
-
 }
 
 // countDown;
